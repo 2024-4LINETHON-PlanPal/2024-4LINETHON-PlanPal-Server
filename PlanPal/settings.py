9317c1eb10b9a22ed27c7ef503fb7d77bbb863a5
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-15 13:13:14 +0900

    Fix : 이미지 경로 https로 수정

--- PlanPal/settings.py
+++ PlanPal/settings.py
@@ -182,8 +182,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'https://planpal.kro.kr/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
