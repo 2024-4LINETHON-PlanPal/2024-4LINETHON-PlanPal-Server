244721163e3c3eb2bdb8ad3274c2c8d385164ca7
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-15 13:37:28 +0900

    Fix : MEDIA_URL 재 설정

--- PlanPal/settings.py
+++ PlanPal/settings.py
@@ -184,7 +184,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = "/static/"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://planpal.kro.kr/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
