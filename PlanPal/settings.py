cbe06c4012debc302f6d1ae2efd2f1a154031d22
mxinseo <110973127+mxinseo@users.noreply.github.com>
2024-11-15 13:24:57 +0900

    Feeat : STATIC_ROOT 설정

--- PlanPal/settings.py
+++ PlanPal/settings.py
@@ -182,8 +182,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'https://planpal.kro.kr/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = "/static/"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
